357ed627dce80389f5637c681431779c9344b28c
XGammer972 <81441088+XGammer972@users.noreply.github.com>
2021-03-26 17:55:16 -0300

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*¡formato incompleto! Ejemplo :*
+*¡formato incorrecto! Elija: *
 
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
